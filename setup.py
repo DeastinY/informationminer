f1900487a06ac417e868cff98cf701b6f306a7bc
DeastinY <polzin.richard@gmail.com>
2017-05-05 15:29:12 +0200

    finished config

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'informationminer',
+  packages = ['informationminer'],
+  version = '1.0',
+  description = 'Automatically performs NLP techniques. Currently supports German and English language.',
+  author = 'Richard Polzin',
+  author_email = 'polzin.richard@gmail.com',
+  url = 'https://github.com/deastiny/informationminer',
+  download_url = 'https://github.com/DeastinY/informationminer/archive/1.0.tar.gz',
+  keywords = ['NLP', 'POS', 'NER', 'NLTK'],
+  classifiers = [],
+)
